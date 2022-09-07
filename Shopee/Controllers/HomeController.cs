bd20d9c80feaf81bfed7175b86aea8c138fba776
Administrator <Administrator@WINDOWS-10>
2022-09-07 14:53:18 +0700

    Add project files.

---
+++ Shopee/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shopee.Models;
+
+namespace Shopee.Controllers
+{
+    public class HomeController : Controller
+    {
+        ShopeeEntities db = new ShopeeEntities();
+        public ActionResult Index()
+        {
+            var product = db.Products.ToList();
+            ViewBag.Product = product;
+            return View();
+        }
+        public ActionResult Details(Guid Id)
+        {
+            return View(db.Products.Find(Id));
+        }
+    }
+}
---
+++ Shopee/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shopee.Models;
+
+namespace Shopee.Controllers
+{
+    public class ProductController : Controller
+    {
+        ShopeeEntities db=new ShopeeEntities();
+        public ActionResult Index(Guid CategoryId)
+        {
+            var item=db.Products.Where(p=>p.CategoryId==CategoryId).ToList();
+            return View(item);
+        }
+    }
+}
